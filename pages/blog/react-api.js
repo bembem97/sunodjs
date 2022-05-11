379afc1d1ce43b7b702b5f65d9c9a8bacbe26de6
bembem97 <bembemcabrera97@gmail.com>
2022-05-11 23:14:38 +0800

    Excerpt not working well in deployment

--- pages/blog/react-api.js
+++ pages/blog/react-api.js
@@ -17,23 +17,29 @@ const ReactAPI = ({ data }) => {
       <LayoutBlog>
         <Container className="px-2 mb-3 rounded flex flex-col gap-2">
           <Typography variant="h2" className="ml-2">
-            API Hook Reference
+            API Hook Reference - EXCERPT NOT WORKING
           </Typography>
 
           {/* //* BLOG LIST: <CARD /> */}
-          {data.map((_data_, index) => {
-            const { title, date } = _data_.data
+          {data.map((article, index) => {
+            const { title, date } = article.data
+            console.log(
+              `%cExcerpt`,
+              `color: emerald; background-color: gray;`,
+              article.excerpt
+            )
 
             return (
               <React.Fragment key={index}>
                 <Card className="bg-secondary">
                   <CardHeader datePosted={date} className="ml-auto" />
-                  <CardBody title={title} titleClassName="text-3xl">
-                    {_data_.excerpt}
-                  </CardBody>
+                  {/* <CardBody title={title} titleClassName="text-3xl">
+                    {article.excerpt}
+                  </CardBody> */}
+                  <p>{article.excerpt}</p>
 
                   <CardFooter>
-                    <Link href={`/blog/react-api/${_data_.slug}`} passHref>
+                    <Link href={`/blog/react-api/${article.slug}`} passHref>
                       <Button tag="a" color="warning">
                         Read more &rarr;
                       </Button>
