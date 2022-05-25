40702ba9507c7547e1b844d12724cfb20418c86b
bembem97 <bembemcabrera97@gmail.com>
2022-05-25 14:31:14 +0800

    a message

--- components/layout/LayoutBlog.js
+++ components/layout/LayoutBlog.js
@@ -5,7 +5,7 @@ import TableOfContents from "components/shared/tableOfContents/TableOfContents"
 
 const LayoutBlog = ({ children, title, Toc, ...props }) => {
   const contentCell = Toc
-    ? "md:col-span-7 lg:col-span-6"
+    ? "md:col-start-1 md:col-end-8 lg:col-span-6"
     : "md:col-span-full lg:col-span-8"
 
   const media = useMediaQuery("lg", true)
@@ -14,13 +14,15 @@ const LayoutBlog = ({ children, title, Toc, ...props }) => {
     <>
       <Layout title={title}>
         <div className="wrapper grid grid-cols-1 md:grid-cols-10">
-          <div className="lg:col-span-2">{media && <SideNav />}</div>
+          <div className={`lg:col-span-2 hidden lg:block`.trim()}>
+            {media && <SideNav />}
+          </div>
 
           <div className={`w-full ${contentCell}`}>{children}</div>
 
           {Toc ? (
             <TableOfContents
-              className={"hidden md:block md:col-span-3 lg:col-span-2"}
+              className={"hidden md:block md:col-span-2 lg:col-span-2"}
             >
               <Toc />
             </TableOfContents>
